f2037ee3c888f2c06e761bd8ad4a116afa3f05de
Chokwaphat <chokwaphat@Chokwaphats-MacBook-Pro.local>
2016-11-02 12:32:34 +0700

    setScreen

---
+++ core/src/Screens/PlayScreen.java
@@ -0,0 +1,72 @@
+package Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.chokwapeem.game.Gogoletsgo;
+
+public class PlayScreen implements Screen{
+	private Gogoletsgo game;
+	Texture texture;
+	private OrthographicCamera gamecam;
+	private Viewport gamePort;
+	
+	public PlayScreen(Gogoletsgo game) {
+		this.game = game;
+		texture = new Texture("badlogic.jpg");
+		gamecam = new OrthographicCamera();
+		gamePort = new FitViewport(800, 480, gamecam);
+	}
+	
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		game.batch.setProjectionMatrix(gamecam.combined);
+		game.batch.begin();
+		game.batch.draw(texture, 0, 0);
+		game.batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		gamePort.update(width, height);
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

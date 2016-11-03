d3d9d8a1edd7c41fb17d1ee977ed5a837a73d122
Chokwaphat <chokwaphat@Chokwaphats-MacBook-Pro.local>
2016-11-03 21:31:47 +0700

    WithMapObject

--- core/src/Screens/PlayScreen.java
+++ core/src/Screens/PlayScreen.java
@@ -1,12 +1,23 @@
 package Screens;
 
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.chokwapeem.game.Gogoletsgo;
@@ -19,6 +30,8 @@ public class PlayScreen implements Screen{
 	private TmxMapLoader maploader;
 	private TiledMap map;
 	private OrthogonalTiledMapRenderer renderer;
+	private World world;
+	private Box2DDebugRenderer b2d;
 	
 	
 	public PlayScreen(Gogoletsgo game) {
@@ -26,9 +39,65 @@ public class PlayScreen implements Screen{
 		gamecam = new OrthographicCamera();
 		gamePort = new FitViewport(Gogoletsgo.WIDTH, Gogoletsgo.HEIGHT, gamecam);
 		maploader = new TmxMapLoader();
-		map = maploader.load("level1.tmx");
+		map = maploader.load("map.tmx");
 		renderer = new OrthogonalTiledMapRenderer(map);
-		gamecam.position.set(gamePort.getScreenWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+		gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+		world = new World(new Vector2(0, 0), true);
+		b2d = new Box2DDebugRenderer();
+		BodyDef bdef = new BodyDef();
+		PolygonShape shape = new PolygonShape();
+		FixtureDef fdef = new FixtureDef();
+		Body body;
+		
+		//create ground
+		for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+			body = world.createBody(bdef);
+			shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+			
+		}
+
+		//create pipes
+		for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+			body = world.createBody(bdef);
+			shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+			
+		}
+		
+		//create bricks
+		for(MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+			body = world.createBody(bdef);
+			shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+			
+		}
+		
+		//create coins
+		for(MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
+			Rectangle rect = ((RectangleMapObject) object).getRectangle();
+			bdef.type = BodyDef.BodyType.StaticBody;
+			bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+			body = world.createBody(bdef);
+			shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+			fdef.shape = shape;
+			body.createFixture(fdef);
+			
+		}
+		
+		
 	}
 	
 	@Override
@@ -51,11 +120,10 @@ public class PlayScreen implements Screen{
 	@Override
 	public void render(float delta) {
 		update(delta);
-		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
-		
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		renderer.render();
+		b2d.render(world, gamecam.combined);
 		
 		
 	}
